e43f774fafdae906adaf446825f564dbb658e07e
Roberto <roberto.popandron@gmail.com>
2025-09-16 20:00:34 +0100

    Fix imports, add supabase client, add basic UI components, install recharts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,7 @@
-export { default as CompanyAdminDashboard } from "./CompanyAdminDashboard";
+export { default as CompanyAdminDashboard } from "./CompanyAdminDashboard.tsx";
+export { default as Overview } from "./Overview.tsx";
+export { default as UsersTab } from "./UsersTab.tsx";
+export { default as AssociationsTab } from "./AssociationsTab.tsx";
+export { default as RequestsTab } from "./RequestsTab.tsx";
+export { default as NotificationsTab } from "./NotificationsTab.tsx";
+export { default as ExportsTab } from "./ExportsTab.tsx";
