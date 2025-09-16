e1a9c79ada7a9fa883700fe5ff33725885067b04
Roberto <roberto.popandron@gmail.com>
2025-09-16 17:02:08 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import CompanyAdminDashboard from "./components/CompanyAdminDashboard";
+import { CompanyAdminDashboard } from "./components";
 
 function App() {
   const testCompanyId = "3ea4379e-4f15-41a0-9c5e-29ce1f807210"; // pune aici un UUID real
