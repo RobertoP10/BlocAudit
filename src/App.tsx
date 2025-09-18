969a383788f24e8bbb713f51f35d4974846e9536
Roberto <roberto.popandron@gmail.com>
2025-09-18 08:28:33 +0100

    Added Sidebar component and updated App.tsx with React Router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
 import SuperAdminDashboard from "./pages/superadmin/SuperAdminDashboard";
 import AdminDashboard from "./pages/admin/AdminDashboard";
 import TechnicalDashboard from "./pages/technical/TechnicalDashboard";
@@ -8,13 +9,21 @@ import ClientDashboard from "./pages/client/ClientDashboard";
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/superadmin" element={<SuperAdminDashboard />} />
-        <Route path="/admin" element={<AdminDashboard />} />
-        <Route path="/technical" element={<TechnicalDashboard />} />
-        <Route path="/service" element={<ServiceDashboard />} />
-        <Route path="/client" element={<ClientDashboard />} />
-      </Routes>
+      <div className="flex">
+        {/* Sidebar fix */}
+        <Sidebar />
+
+        {/* Conținut pagină */}
+        <main className="flex-1 bg-gray-50 p-6">
+          <Routes>
+            <Route path="/superadmin" element={<SuperAdminDashboard />} />
+            <Route path="/admin" element={<AdminDashboard />} />
+            <Route path="/technical" element={<TechnicalDashboard />} />
+            <Route path="/service" element={<ServiceDashboard />} />
+            <Route path="/client" element={<ClientDashboard />} />
+          </Routes>
+        </main>
+      </div>
     </Router>
   );
 }
